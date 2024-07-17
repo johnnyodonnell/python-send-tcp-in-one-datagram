bf64479faed554c5acbe8c09c26c2a079fb42272
Johnny O'Donnell <odonnell_johnny@yahoo.com>
2024-07-16 17:51:33 -0700

    Use more accurate `carry_over` method

--- lib/TCP_Segment.py
+++ lib/TCP_Segment.py
@@ -13,10 +13,14 @@ def print_byte_string(byte_str):
             word = ""
 
 def carry_over(value):
-    while (value & 0xFFFF0000) > 0:
-        value = (value & 0xFFFF) + ((value & 0xFFFF0000) >> 16)
+    top_bits = value >> 16
+    while top_bits > 0:
+        value = (value & 0xFFFF) + top_bits
     return value
 
+# Received help from:
+# - https://gist.github.com/david-hoze/0c7021434796997a4ca42d7731a7073a
+# - https://inc0x0.com/tcp-ip-packets-introduction/tcp-ip-packets-3-manually-create-and-send-raw-tcp-ip-packets/
 def calculate_checksum(byte_str):
     total = 0
     for i in range(0, len(byte_str), 2):
